326b7f84378d1b4f9e85d26c9482c70f08cb249a
Viktor Fernandez <vicantonfernandez@gmail.com>
2025-10-19 17:39:50 +0800

    Batch file to create workspace automatically

---
+++ create_workspace.bat
@@ -0,0 +1,5 @@
+@echo off
+echo === MCBE Workspace Initializer ===
+echo === You can change the file name by editing this .bat file ===
+python generate_workspace.py --name "Pack Name" --desc "Pack Description"
+pause
